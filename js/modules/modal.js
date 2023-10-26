07d7cecd345f58deb818d1add2ecb595783bec8e
Bruno <bdorigon@hotmail.com>
2023-10-26 18:22:23 -0300

    modal_v1

---
+++ js/modules/modal.js
@@ -0,0 +1,18 @@
+export default function modal() {}
+
+const botaoAbrir = document.querySelector(".abrir");
+const botaoFechar = document.querySelector(".fechar");
+const containerModal = document.querySelector(".container");
+const inputEmail = document.getElementById("email");
+
+botaoAbrir.addEventListener("click", abrirModal);
+botaoFechar.addEventListener("click", fecharModal);
+
+function abrirModal() {
+  containerModal.classList.add("ativo");
+  inputEmail.focus();
+}
+
+function fecharModal() {
+  containerModal.classList.remove("ativo");
+}
